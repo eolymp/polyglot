c0f0631293fe72455e055c64b28871647a875c84
Anton Tsypko <antontsypko@gmail.com>
2022-07-28 17:06:43 +0200

    Update polygon.go

--- cmd/eolymp-polyglot/types/polygon.go
+++ cmd/eolymp-polyglot/types/polygon.go
@@ -57,18 +57,18 @@ func CreatePolygonImporter(path string) (*PolygonImporter, error) {
 }
 
 var mapping = map[string][]string{
-	"gpp":    {"c.gcc", "cpp.g++", "cpp.g++11", "cpp.g++14", "cpp.g++17", "cpp.ms", "cpp.msys2-mingw64-9-g++17"},
-	"csharp": {"csharp.mono"},
-	"d":      {"d"},
-	"go":     {"go"},
-	"java":   {"java11", "java8"},
-	"kotlin": {"kotlin"},
-	"fpc":    {"pas.dpr", "pas.fpc"},
-	"php":    {"php.5"},
-	"python": {"python.2", "python.3"},
-	"pypy":   {"python.pypy2", "python.pypy3"},
-	"ruby":   {"ruby"},
-	"rust":   {"rust"},
+	"cpp:17-gnu10": {"c.gcc", "cpp.g++", "cpp.g++11", "cpp.g++14", "cpp.g++17", "cpp.ms", "cpp.msys2-mingw64-9-g++17"},
+	"csharp":       {"csharp.mono"},
+	"d":            {"d"},
+	"go":           {"go"},
+	"java":         {"java11", "java8"},
+	"kotlin":       {"kotlin"},
+	"fpc":          {"pas.dpr", "pas.fpc"},
+	"php":          {"php.5"},
+	"python":       {"python.2", "python.3"},
+	"pypy":         {"python.pypy2", "python.pypy3"},
+	"ruby":         {"ruby"},
+	"rust":         {"rust"},
 }
 
 func (p PolygonImporter) GetVerifier() (*executor.Verifier, error) {
