6a445e6e1d6955a52e253943a659dc1b11a091e3
Sergey Kolodyazhnyy <sergey.kolodyazhnyy@gmail.com>
2023-03-01 18:15:50 +0100

    Do not return an error for status codes in 2xx range

--- cmd/httpx/retry.go
+++ cmd/httpx/retry.go
@@ -29,7 +29,7 @@ func WithRetry(retries int) func(Client) Client {
 					return nil, err
 				}
 
-				if resp.StatusCode != http.StatusOK {
+				if int(resp.StatusCode/100) != 2 {
 					body, _ = ioutil.ReadAll(resp.Body)
 					log.Printf("Server returned an error, status code %d: %s", resp.StatusCode, body)
 					time.Sleep(time.Second * time.Duration((attempt+1)*(attempt+1)))
